9b9f989318989b38e5a31f2d0e60369132c4b2ac
JasonMAnderson <jasonanderson@live.com>
2023-04-13 08:53:51 -0600

    config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Nibiru-Network', // Usually your GitHub org/user name.
-  projectName: 'Documentation', // Usually your repo name.
+  projectName: 'nibiru-docs.github.io', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
