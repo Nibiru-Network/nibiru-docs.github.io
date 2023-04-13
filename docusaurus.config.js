c6c47c46fb82ef51b570299e38d125c5c6c36e46
JasonMAnderson <jasonanderson@live.com>
2023-04-13 09:01:32 -0600

    prod url change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,8 +11,8 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  // url: 'https://nibiru-docs.github.io',
-  url: 'https://docs.nibirunet.io',
+  url: 'https://nibiru-docs.github.io',
+  // url: 'https://docs.nibirunet.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
